07205138fde67edb0aea10637d5ca3be8e56bb7c
mohan <mohan@LAPTOP-B3GG8RMS>
2022-07-18 16:04:35 +0530

    [MOHAN]:2nd changes in code

---
+++ src/demo11.java
@@ -0,0 +1,16 @@
+
+public class demo11 
+{
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("ma");
+		System.out.println("thuze");
+		System.out.println("salam");
+		
+		
+		
+	}
+	
+
+}
